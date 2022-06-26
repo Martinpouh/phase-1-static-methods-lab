afdfa8039cd1598299d19ef597cf1faa36e1893b
Martinpouh <martinchege2015@gmail.com>
2022-06-26 23:24:27 +0300

    commit tree bd65dbac5c645fd058b65202a9e2237aa3f7f8e7

--- index.js
+++ index.js
@@ -1,3 +1,28 @@
 class Formatter {
-  //add static methods here
+
+    static capitalize(string) {
+        return string.charAt(0).toUpperCase() + string.slice(1)
+    }
+
+    static sanitize(string) {
+        return string.replace(/[^A-Za-z0-9- ']+/g, "")
+    }
+
+    static titleize(string) {
+        let newArray = []
+        const stringArray = string.split(' ')
+        const exceptW = ['the', 'a', 'an', 'but', 'of', 'and', 'for', 'at', 'by', 'from']
+        stringArray.forEach(word => {
+            if (exceptW.indexOf(word) !== -1) {
+                if (stringArray.indexOf(word) === 0) {
+                    newArray.push(this.capitalize(word))
+                } else {
+                    newArray.push(word)
+                }
+            } else {
+                newArray.push(this.capitalize(word))
+            }
+        })
+        return newArray.join(' ')
+    }
 }
